13df1db86d26dca8adb03f7904f16b1ab5657a83
SFMSupersanta <otakuaria4710@gmail.com>
2022-01-13 21:45:55 +0700

    Update tempCodeRunnerFile.c

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,4 @@
- 
+nt randomNum = rand() % 6;                             //ran number created is from 0 to 5(6 in total)
+     // printf("The radnum is: %d\n", randomNum);                //check code for dev only
+    int reset=1;
+    while (rese
