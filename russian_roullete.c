9221bae5ec55fcb9d2c7fca2e20fb6127d3acec3
SFMSupersanta <otakuaria4710@gmail.com>
2022-01-15 13:24:34 +0700

    added some lines
    re organized code to better understand how it works

--- russian_roullete.c
+++ russian_roullete.c
@@ -16,35 +16,38 @@ void multi_play()                                          //multi_play function
 {
   printf("2 players: type 2 \t\t3 players: type 3 \t\t4 players: type 4 \t\t5 players: type 5 \n");
 
-  printf("Your choice: ");                 //number of players (1)
+  printf("Your choice: ");                                 //number of players (1)
 
   int randomNum;
   int players =1;
-  char TempString[255];
-  char *player_name[4];
+  char TempString[255];                                  //temp string to store a line of string in before dup to player name string
+  char *player_name[4];                                  //player name string
 
-while (players==1)
+while (players==1)                                       //loop to get answer form 2 to 5
 {
 scanf("%d", &players) ;
-if (players<=5 && players>1)                         //enter player's name 
+if (players<=5 && players>1)                              //enter player's name 
   { 
   for(int i = 0; i < players; i++)
   {
+
     printf("Type in #%d player's name: ", i+1);
-    
-    scanf(" %[^\n]",&TempString) ; 
+    scanf(" %[^\n]",&TempString) ;                        //put space before %[^\n] to get a line of strings
     player_name[i] = strdup(TempString); 
+
   }
   } else 
     {
-        printf("At least 2 or more people must join and no more than 5 people allowed, please enter a valid number if you want to take the challenge:\nYour choice: ");
+
+        printf("At least 2 or more people must join and no more than 5 people allowed, please enter a valid number if you want to take the challenge:\nYour choice: ");  
         players=1;
+        
     }  
 }
   int again=1;                                           // initialize the value to start the game
   while(again == 1)
   {
-  //mpstart:
+
   randomNum = rand() % 6;                             //ran number created is from 0 to 5(6 in total)    
       //printf("The radnum is: %d\n", randomNum);                //check code for dev only 
       int temp_player = 0;                                        //variable for displaying player's name
@@ -52,79 +55,81 @@ if (players<=5 && players>1)                         //enter player's name
 
       for(int i = 1; i <= 6; i++) 
       {
+
         printf(" Player %s's Turn \nPress ENTER to test your luck!\n", player_name[temp_player]);
+
         eliminated_player = temp_player;
+        
         temp_player++;
 
         if(temp_player >= players) 
           temp_player = 0;
+
           _getch();                                             //pause program until the user press a button
 
         if (i - 1 == randomNum)
         {
+
           printf("\tYOU DIED\n\t-------- \n\n Player %s has been eliminated!\n The others players wins\n", player_name[eliminated_player]);
           printf("\nEnter 1 to continue, any other number to return to main menu: ");
-          scanf("%d", &again);  
-                      
+          scanf("%d", &again);              
           break;  
-            /********************************  
-              int again;
-              scanf("%d", &again);
-            
-              switch (again)
-              {
-                case 1: 
-                goto mpstart;
-
-                default: 
-                printf("\nGG\n\n"); 
-                break;
-              
-              }
-            ********************************/                                           
+                                                    
         }
 
-            
         printf("Still alive eh? \n");
          
       }
+
   }
 
 }
 
 void play_single()                                        //single-play function
 {
-  int randomNum = rand() % 6;                             //ran number created is from 0 to 5(6 in total)
-     // printf("The radnum is: %d\n", randomNum);                //check code for dev only
-    int reset=1;
-    while (reset==1)
-    {  
-      for(int i = 1; i <= 6; i++) 
-      {
-        printf(" Turn %d \nPress ENTER to test your luck!\n", i);
-        _getch();                                             //pause program until the user press a button
-        
-        if (i - 1 == randomNum)
-        {
-          printf("\tYOU DIED\n\t-------- \n\nYou have survied %d times this day!", i);
 
-          if(i <= 5) 
-              {
-              printf("your luck sucks, go do sth good then try again\n");
-              printf("\nEnter 1 to continue, any other number to return to main menu: ");
-              scanf("%d",&reset);
-              }  
-          else if(i > 5)
-              { 
-              printf("CONGRATULATIONS, YOU WON.");
-              printf("\nEnter 1 to continue, any other number to return to main menu: ");
-              scanf("%d",&reset);
-              }
-          break;                                              //basicly the whole function
-        }
-        printf("Still alive eh? \n");
+  int randomNum = rand() % 6;                             //ran number created is from 0 to 5(6 in total)
+  // printf("The radnum is: %d\n", randomNum);                //check code for dev only
+  int reset=1;
+
+  while (reset==1)
+  { 
+
+    for(int i = 1; i <= 6; i++) 
+    {
+      printf(" Turn %d \nPress ENTER to test your luck!\n", i);
+      _getch();                                             //pause program until the user press a button
+      
+      if (i - 1 == randomNum)
+      {
+
+        printf("\tYOU DIED\n\t-------- \n\nYou have survied %d times this day!", i);
+
+        if(i <= 5) 
+            {
+
+            printf("your luck sucks, go do sth good then try again\n");
+            printf("\nEnter 1 to continue, any other number to return to main menu: ");
+            scanf("%d",&reset);
+
+            }  
+        else if(i > 5)
+            { 
+
+            printf("CONGRATULATIONS, YOU WON.");
+            printf("\nEnter 1 to continue, any other number to return to main menu: ");
+            scanf("%d",&reset);
+
+            }
+        break; 
+                                                     //basicly the whole function
       }
+      printf("Still alive eh? \n");
+
     }
+
+  }
+
 }
 
 void russianroulette()
@@ -136,6 +141,7 @@ void russianroulette()
   
   switch(game)
   {
+
     case 1:
       play_single();
       break;
@@ -143,6 +149,7 @@ void russianroulette()
     case 0:
       multi_play();
     break;
+
   }
 }
   
@@ -156,6 +163,7 @@ int main()
   
   do                                                    //do-while loop
   {
+
     printf("\n\n\t----------------------------------------------------\n\tEnter 1 to try your luck, Enter 0 if you are a wimp: ");
     scanf("%d", &input);
     printf("\n\n");
@@ -163,18 +171,30 @@ int main()
     switch(input)
     {
       case 0:
-        printf("GTFO you weak ass mf \n");
-        printf("Press ENTER to get tf outta here");
+      {
 
-        _getch();                                             //pause program until the user press a button
-        break;
-      
+        printf("GTFO you weak ass mf \n");
+        break;  
+
+      }  
       case 1:
+      {
+
         russianroulette();
         break;
+
+      }
+
     }
+
   }
+
   while(input!=0);
+
+  printf("Press ENTER to get tf outta here");
+  _getch();                                             //pause program until the user press a button
+
+  
   return 0;
 
 }
@@ -186,4 +206,5 @@ DONE: -Fix multiplayer function where there is 2 players
       -Multiplayer loop. Entering names each play is annoying as fuck
       -Add retry to single-player function
 Changelog: makes code a bit easier to read
+           re organized code to better understand how it works
 */
