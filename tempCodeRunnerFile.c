9d1e860f41ae059add98d29e6027beb04de84c82
Arcahv <97589681+Arcahv@users.noreply.github.com>
2022-01-15 14:38:49 +0700

    Delete tempCodeRunnerFile.c

--- tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
-nt randomNum = rand() % 6;                             //ran number created is from 0 to 5(6 in total)
-     // printf("The radnum is: %d\n", randomNum);                //check code for dev only
-    int reset=1;
-    while (rese
